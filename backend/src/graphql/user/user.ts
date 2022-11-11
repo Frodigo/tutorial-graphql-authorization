fd2989a9d421b668168babc4eb27d38a41ce1429
Marcin Kwiatkowski <marcin@frodigo.com>
2022-11-11 06:26:37 +0100

    feat: connect resolvers with database

--- backend/src/graphql/user/user.ts
+++ backend/src/graphql/user/user.ts
@@ -1,5 +1,5 @@
-import { extendType, objectType, nonNull, stringArg } from "nexus";
-import { NexusGenObjects } from "../../../nexus-typegen";
+import {extendType, nonNull, objectType, stringArg} from "nexus";
+import {NexusGenObjects} from "../../../nexus-typegen";
 
 export const User = objectType({
     name: "User",
@@ -12,30 +12,13 @@ export const User = objectType({
     },
 });
 
-let users: NexusGenObjects['User'][] = [
-    {
-        id: 1,
-        firstName: 'Marcin',
-        lastName: 'Kwiatkowski',
-        email: 'marcin@abc.xyz',
-        password: 'admin123'
-    },
-    {
-        id: 1,
-        firstName: 'Jan',
-        lastName: 'Kowalski',
-        email: 'jan@abc.xyz',
-        password: 'admin1234'
-    }
-]
-
 export const UserQuery = extendType({
     type: "Query",
     definition(t) {
         t.nonNull.list.nonNull.field("users", {   // 3
             type: "User",
             resolve(parent, args, context, info) {    // 4
-                return users;
+                return context.prisma.user.findMany()
             },
         });
     },
@@ -56,18 +39,14 @@ export const LinkMutation = extendType({
             resolve(parent, args, context) {
                 const { firstName, lastName, email, password } = args;
 
-                let idCount = users.length + 1;
-                const user = {
-                    id: idCount,
-                    firstName,
-                    lastName,
-                    email,
-                    password
-                };
-
-                users.push(user);
-
-                return user;
+                return context.prisma.user.create({
+                    data: {
+                        firstName,
+                        lastName,
+                        email,
+                        password
+                    },
+                });
             },
         });
     },
